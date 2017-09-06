534163dcc208cb5f011d1a1d2a80132e54c2d64e
mitzy <mitzy@192.168.22.73>
2017-09-06 10:26:50 -0700

    practicas; fecha:martes 06 de sep de 2017

---
+++ src/ejemplos/Ejemplos.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplos;
+
+/**
+ *
+ * @author mitzy
+ */
+public class Ejemplos {
+
+    private static Object leer;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int ingresado=0; 
+        int pares=0;
+        int suma=0;
+        
+       while(pares<5);
+       {
+           System.out.println("ingresa numero");
+           ingresado=leer.nextInt();
+         if (ingresado %2==0);
+         {
+             pares++;
+         }
+         if (ingresado >=20);
+         {
+             suma= ingresado + suma;
+         }
+           
+       }
+        
+       
+    }
+    
+}
